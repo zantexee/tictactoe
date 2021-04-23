13517fa329e892f221a9323e0d4e67ed05d8c2b8
Flowertin Madalin <60981000+zantexee@users.noreply.github.com>
2021-04-24 01:45:33 +0300

    pick box and render

--- js/main.js
+++ js/main.js
@@ -1,25 +1,36 @@
 const gameBoard = (() => {
-  const positionArray = [
-    ['X', '0'],
-    ['X', '1'],
-    ['X', '2'],
-    ['O', '3'],
-    ['O', '4'],
-    ['O', '5'],
-    ['X', '6'],
-    ['X', '7'],
-    ['X', '8'],
-  ];
-  return { positionArray };
+  const positionArray = [];
+  const addPosition = (sign, divNumber) => {
+    const arr = [sign, divNumber];
+    positionArray.push(arr);
+  };
+
+  return { positionArray, addPosition };
 })();
 
 const displayController = (() => {
   const gameBoardDiv = document.getElementById('gameboard');
 
   const boxDivs = gameBoardDiv.getElementsByClassName('gamebox');
+  const boxDivsArr = Array.prototype.slice.call(boxDivs);
 
-  //dummy render
-  gameBoard.positionArray.forEach((pair) => {
-    boxDivs[pair[1]].textContent = pair[0];
-  });
+  boxDivsArr.forEach((div, divNumber) =>
+    div.addEventListener('click', () => {
+      const arrLength = gameBoard.positionArray.length;
+      const lastSign = arrLength > 0 ? gameBoard.positionArray[arrLength - 1][0] : 'X';
+      const currSign = lastSign === 'X' && arrLength > 0 ? 'O' : 'X';
+      if (!div.textContent) div.textContent = currSign;
+
+      gameBoard.addPosition(currSign, divNumber);
+    }),
+  );
 })();
+
+const player = () => {
+  const playerSign = gameBoard.positionArray.length > 0 ? 'O' : 'X';
+
+  return { playerSign };
+};
+
+const player1 = player();
+const player2 = player();
