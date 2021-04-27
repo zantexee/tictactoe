82832def8683b70cb9bcfb753033b0d0fa1f36d5
Flowertin Madalin <60981000+zantexee@users.noreply.github.com>
2021-04-27 21:24:05 +0300

    refactored display logic

--- js/main.js
+++ js/main.js
@@ -1,11 +1,46 @@
 const gameBoard = (() => {
-  const positionArray = [];
-  const addPosition = (sign, divNumber) => {
-    const arr = [sign, divNumber];
-    positionArray.push(arr);
+  const positionArray = [
+    [' ', '0'],
+    [' ', '1'],
+    [' ', '2'],
+    [' ', '3'],
+    [' ', '4'],
+    [' ', '5'],
+    [' ', '6'],
+    [' ', '7'],
+    [' ', '8'],
+  ];
+  let currentSign = 'X';
+  let currentMove = 0;
+
+  const changeCurrentSign = () => {
+    if (gameBoard.currentMove !== 0) {
+      gameBoard.currentSign = gameBoard.currentSign === 'X' ? 'O' : 'X';
+      return gameBoard.currentMove++;
+    }
+    return gameBoard.currentMove++;
   };
 
-  return { positionArray, addPosition };
+  const setPosition = (sign, divNumber) => {
+    positionArray[divNumber][0] = sign;
+  };
+
+  const checkWinner = () => {
+    const xArr = positionArray.filter((arr) => arr[0] === 'X');
+    const oArr = positionArray.filter((arr) => arr[0] === 'O');
+
+    let winCond = [];
+    xArr.forEach((arr) => winCond.push(arr[2]));
+  };
+
+  return {
+    positionArray,
+    currentMove,
+    currentSign,
+    setPosition,
+    checkWinner,
+    changeCurrentSign,
+  };
 })();
 
 const displayController = (() => {
@@ -16,12 +51,9 @@ const displayController = (() => {
 
   boxDivsArr.forEach((div, divNumber) =>
     div.addEventListener('click', () => {
-      const arrLength = gameBoard.positionArray.length;
-      const lastSign = arrLength > 0 ? gameBoard.positionArray[arrLength - 1][0] : 'X';
-      const currSign = lastSign === 'X' && arrLength > 0 ? 'O' : 'X';
-      if (!div.textContent) div.textContent = currSign;
-
-      gameBoard.addPosition(currSign, divNumber);
+      gameBoard.changeCurrentSign();
+      if (!div.textContent) div.textContent = gameBoard.currentSign;
+      gameBoard.setPosition(gameBoard.currentSign, divNumber);
     }),
   );
 })();
